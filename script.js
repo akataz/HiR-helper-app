20a70cd7f829ff7cda2857f5bf1a9cb6b45cbf63
Ryo Wheatley <ryo.capricorn92@gmail.com>
2016-09-11 23:57:07 -0400

    (feat) add refocus on input entry

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ var createClass = function () {
   addStudent = function () {
     var student = $('#student').val();
     $('#student').val('');
+    $('#student').focus();
     console.log(student);
     $('#newClassTable').append('<tr><td>' + student + '</td></tr>').addlistener;
     studentsArr.push(student);
